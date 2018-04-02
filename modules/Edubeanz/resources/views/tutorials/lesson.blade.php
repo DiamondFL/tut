7726eb424af494f2e2a7ecef710580b525dd4b7d
Fight Light Diamond <i.am.m.cuong@gmail.com>
2018-04-02 23:38:03 +0700

    0k

--- modules/Edubeanz/resources/views/tutorials/lesson.blade.php
+++ modules/Edubeanz/resources/views/tutorials/lesson.blade.php
@@ -42,6 +42,9 @@
                 </div>
 
             </div>
+            <div id="comments-container">
+
+            </div>
         </div>
         <div class="col-lg-3">
             <ul class="list-group">
@@ -58,11 +61,85 @@
     <!-- Footer Widgets -->
 @endsection
 @push('head')
+    <link rel="stylesheet" type="text/css" href="{{asset('bower_components/jquery-comments')}}/css/jquery-comments.css">
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="{{asset('')}}assets/css/neon-core.css">
 @endpush
 @push('js')
     <script src="{{asset('')}}assets/js/jquery-ui/js/jquery-ui-1.10.3.minimal.min.js"></script>
+    {{--<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>--}}
+    <script type="text/javascript" src="{{asset('bower_components/jquery-comments')}}/js/jquery-comments.js"></script>
     <script src="{{asset('')}}assets/js/neon-api.js"></script>
     <script src="{{asset('')}}assets/js/neon-demo.js"></script>
     <script src="{{asset('')}}assets/js/neon-chat.js"></script>
+    <script>
+        $('#comments-container').comments({
+            getComments: function(success, error) {
+                var commentsArray = [
+
+                    {
+                        id: 1,
+                        created: '2015-10-01',
+                        content: 'Lorem ipsum dolort sit amet',
+                        fullname: 'Simon Powell',
+                        upvote_count: 2,
+                        user_has_upvoted: false
+                    },{
+                        id: 2,
+                        created: '2015-10-01',
+                        content: 'Lorem ipsum dolort sit amet',
+                        fullname: 'Simon Powell',
+                        upvote_count: 2,
+                        user_has_upvoted: false
+                    },{
+                        id: 3,
+                        created: '2015-10-01',
+                        content: 'Lorem ipsum dolort sit amet',
+                        fullname: 'Simon Powell',
+                        upvote_count: 2,
+                        user_has_upvoted: false
+                    },
+                ];
+                success(commentsArray);
+            }
+        });
+        $('#comments-container').comments({
+            getComments: function(success, error) {
+                var commentsArray = [
+
+                    {
+                        id: 1,
+                        created: '2015-10-01',
+                        content: 'Lorem ipsum dolort sit amet',
+                        fullname: 'Simon Powell',
+                        upvote_count: 2,
+                        user_has_upvoted: false
+                    },{
+                        id: 2,
+                        created: '2015-10-01',
+                        content: 'Lorem ipsum dolort sit amet',
+                        fullname: 'Simon Powell',
+                        upvote_count: 2,
+                        user_has_upvoted: false
+                    },{
+                        id: 3,
+                        created: '2015-10-01',
+                        content: 'Lorem ipsum dolort sit amet',
+                        fullname: 'Simon Powell',
+                        upvote_count: 2,
+                        user_has_upvoted: false
+                    },
+                ];
+                success(commentsArray);
+            },
+            fieldMappings: {
+                id: '12',
+                parent: '1',
+                modified: '2015-10-01',
+                fullname: 'name',
+                profilePictureURL: 'user_image',
+                upvoteCount: 'upvotes',
+            }
+        });
+    </script>
 @endpush
